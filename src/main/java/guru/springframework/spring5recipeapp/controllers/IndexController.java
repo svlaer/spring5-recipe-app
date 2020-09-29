091e29c54ff4c650608aad6f1918ef421ec635fe
Seppe Van Laer <25950796+svlaer@users.noreply.github.com>
2020-09-29 11:02:05 +0200

    Set up 1-1 relationships

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5recipeapp;
+package guru.springframework.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
