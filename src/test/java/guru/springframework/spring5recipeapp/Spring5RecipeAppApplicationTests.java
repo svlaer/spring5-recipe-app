4dd83235374ae2049e7efed43a8ff187114756fa
Seppe Van Laer <25950796+svlaer@users.noreply.github.com>
2020-11-30 22:06:08 +0100

    Write first tests with JUnit4

--- src/test/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -1,13 +1,17 @@
 package guru.springframework.spring5recipeapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class Spring5RecipeAppApplicationTests {
+public class Spring5RecipeAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
